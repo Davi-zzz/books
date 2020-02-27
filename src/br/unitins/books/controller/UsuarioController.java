15c8799cb292d39dfb93f71cfacf25289a1eff15
davis <davis@172.16.106.101>
2020-02-27 11:27:43 -0300

    FIM DA AULA. TODAS AS FUNÇÕES FUNCIONANDO

--- src/br/unitins/books/controller/UsuarioController.java
+++ src/br/unitins/books/controller/UsuarioController.java
@@ -1,9 +1,12 @@
 package br.unitins.books.controller;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+
 
 @Named
 @ViewScoped
@@ -15,8 +18,35 @@ public class UsuarioController implements Serializable {
 	private static final long serialVersionUID = 6735972047454490532L;
 	
 	private Usuario usuario;
+	
+	private List<Usuario> listaUsuario;
+	
+	public void editar(Usuario user) {
+		setUsuario(user);
+	}
+	
+	public void incluir() {
+		getListaUsuario().add(getUsuario());
+		limpar();
+	}
+	
+	public void alterar() {
+		System.out.println(getUsuario().getNome());
+	}
+	public void remover() {
+	getListaUsuario().remove(getUsuario());
+	limpar();
+	}
+	public void limpar() {
+		usuario = null;
+	}
 
 	public Usuario getUsuario() {
+		if (usuario == null) {
+			usuario = new Usuario();
+		}
+		
+		
 		return usuario;
 	}
 
@@ -25,9 +55,20 @@ public class UsuarioController implements Serializable {
 		
 	}
 	
-	public void incluir() {
-		System.out.println(getUsuario().getNome());
+
+	public List<Usuario> getListaUsuario() {
+		if (listaUsuario == null) {
+			listaUsuario = new  ArrayList<Usuario>();
+		}
+		
+		return listaUsuario;
 	}
+
+	public void setListaUsuario(List<Usuario> listaUsuario) {
+		this.listaUsuario = listaUsuario;
+	}
+	
+	
 	
 	
 }
